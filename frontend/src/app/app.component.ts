683692507bc5634706ba92a5ce9b6efeccaa847a
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-01 15:28:57 +0100

    Main app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavigationModule } from './modules/navigation/navigation.module';
+import { EasyUIModule } from '@azaber/ngeasy-ui';
+
+import { SocialsComponent } from './components/socials/socials.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    NavigationModule,
+    EasyUIModule,
+    SocialsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
+  title: string = 'Azaber portfolio';
 }
