719c4ece5a00c17fee85fa4ab6d38fdc505b8a24
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-06 19:38:06 +0100

    Create baseUrl.ts

---
+++ frontend/src/app/utils/baseUrl.ts
@@ -0,0 +1,5 @@
+const portfolioUrl: string =
+  'https://portfolio-backend.azaber.com/wp-json/acf/v3';
+const blogUrl: string = 'https://blog-backend.azaber.com/wp-json/wp/v2';
+
+export { portfolioUrl, blogUrl };
