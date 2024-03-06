168a7baeb7f4572f4da74ff917d0cc766989a4a8
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-06 17:50:17 +0100

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MoreProjectsComponent } from './pages/more-projects/more-projects.component';
+import { SingleExperienceComponent } from './pages/single-experience/single-experience.component';
+import { SingleProjectComponent } from './pages/single-project/single-project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'more-projects',
+    pathMatch: 'full',
+    component: MoreProjectsComponent,
+  },
+  {
+    path: 'experience/:id',
+    pathMatch: 'full',
+    component: SingleExperienceComponent,
+  },
+  {
+    path: 'project/:id',
+    pathMatch: 'full',
+    component: SingleProjectComponent,
+  },
+];
