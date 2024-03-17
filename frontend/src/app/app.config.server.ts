c70f3eb002e61f4ba1a10541441e1622baf46706
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-17 23:39:56 +0100

    Update app.config.server.ts

--- frontend/src/app/app.config.server.ts
+++ frontend/src/app/app.config.server.ts
@@ -2,9 +2,15 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app.routes';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), provideHttpClient()],
+  providers: [
+    provideServerRendering(),
+    provideHttpClient(),
+    provideRouter(routes, withHashLocation()),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
