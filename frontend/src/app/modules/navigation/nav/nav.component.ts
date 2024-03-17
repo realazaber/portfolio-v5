d6a876f6679f5e509f60ebcd52c80b348adc2236
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-18 00:01:30 +0100

    Bugfix: main nav

--- frontend/src/app/modules/navigation/nav/nav.component.ts
+++ frontend/src/app/modules/navigation/nav/nav.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoNav } from '../../../components/logos/nav/nav.component';
 import { EasyUIModule } from '@azaber/ngeasy-ui';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, LogoNav, EasyUIModule],
+  imports: [CommonModule, LogoNav, EasyUIModule, RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
