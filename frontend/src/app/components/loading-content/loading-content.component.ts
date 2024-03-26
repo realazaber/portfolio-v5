9873ff203e9ba5195b82f0db2d4cea31bc639562
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-26 21:13:49 +0100

    Feature: component to display during loading

---
+++ frontend/src/app/components/loading-content/loading-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { EasyUIModule } from '@azaber/ngeasy-ui';
+
+@Component({
+  selector: 'app-loading-content',
+  standalone: true,
+  imports: [EasyUIModule],
+  templateUrl: './loading-content.component.html',
+  styles: ``,
+})
+export class LoadingContentComponent {}
