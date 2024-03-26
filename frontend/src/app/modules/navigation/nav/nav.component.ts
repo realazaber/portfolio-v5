7845d79f70ec907e33deefe43a316e14bffb7767
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-26 21:38:34 +0100

    Bug fix: Links go back to home page when on other pages

--- frontend/src/app/modules/navigation/nav/nav.component.ts
+++ frontend/src/app/modules/navigation/nav/nav.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { LogoNav } from '../../../components/logos/nav/nav.component';
 import { EasyUIModule } from '@azaber/ngeasy-ui';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { ModeToggleComponent } from '../../../components/mode-toggle/mode-toggle.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, LogoNav, EasyUIModule, RouterModule],
+  imports: [
+    CommonModule,
+    LogoNav,
+    EasyUIModule,
+    RouterModule,
+    ModeToggleComponent,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
@@ -16,5 +23,16 @@ export class DesktopnavComponent {
 
   toggleMobileMenu(): void {
     this.showMobile = !this.showMobile;
+  }
+
+  constructor(
+    private router: Router,
+    private location: Location,
+  ) {}
+
+  routeHome(): void {
+    if (this.location.path() != '/') {
+      this.router.navigate(['/']);
+    }
   }
 }
