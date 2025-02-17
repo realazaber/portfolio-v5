9ae3751e72d6558f77e8abf6b464a9bfb4425d0a
AZ <47402666+realazaber@users.noreply.github.com>
2025-02-17 14:50:50 +1100

    Feature: Added carousel to testimonials

--- frontend/src/app/components/testimonial/testimonial.component.ts
+++ frontend/src/app/components/testimonial/testimonial.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ITestimonial } from '../../models/testimonial';
 import { EasyUIModule } from '@azaber/ngeasy-ui';
 
@@ -10,6 +10,22 @@ import { EasyUIModule } from '@azaber/ngeasy-ui';
   styles: ``,
   schemas: [],
 })
-export class TestimonialCardComponent {
-  @Input() testimonial: ITestimonial | null = null;
+export class TestimonialCardComponent implements OnInit {
+  @Input() cardIndex: number = {} as number;
+  @Input() testimonial: ITestimonial = {} as ITestimonial;
+  @Input() length: number = {} as number;
+  backNum: number = {} as number;
+  nextNum: number = {} as number;
+
+  ngOnInit(): void {
+    this.backNum = this.cardIndex - 1;
+    this.nextNum = this.cardIndex + 1;
+  }
+
+  goToSlide(slideNum: number, event: Event) {
+    event.preventDefault(); // Prevent default anchor scrolling
+    document
+      .getElementById(`slide${slideNum}`)
+      ?.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+  }
 }
